4a466c9ca5782db22d9f6b2876fe8fb92f9da45b
yuhwan <khfbvxz@naver.com>
2021-06-27 11:11:08 +0900

    중요함

--- javaJS/src/javaJS/JSch61.java
+++ javaJS/src/javaJS/JSch61.java
@@ -15,5 +15,15 @@ class JSch61{
 		System.out.println("t1의 channel값은"+ t1.channel + "입니다.");
 		System.out.println("t2의 channel값은"+ t2.channel + "입니다.");
 		
+		/* 객체 배열 == 참조변수 배열
+		 * Tv tv1. tv2, tv3;   ->  
+		 *  Tv[] tvArr = new Tv[3]; //길이가 3인 Tv타입의 참조변수 배열
+		 * // 객체를 생성해서 배열의 각 요소의 저장
+		 * tvArr[0] = new Tv();	
+		 * tvArr[1] = new Tv();	  // 안에다 객체들을 만들어야됨!! 까먹지 말것
+		 * tvArr[2] = new Tv();	
+		 * 
+ 		 * Tv[] tvArr = {new Tv(), new Tv(), new Tv() }; // 위의 3문장을 간단히 
+		 */
 	}
 }
